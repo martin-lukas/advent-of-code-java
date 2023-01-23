0574b3314ab25822d555829c24cbdcf01f0d2596
Martin Lukas <martinlukas24@protonmail.com>
2023-01-23 20:15:19 +0000

    Extend FileUtils with lines methods

--- src/main/java/org/lukas/adventofcode2022/utils/FileUtils.java
+++ src/main/java/org/lukas/adventofcode2022/utils/FileUtils.java
@@ -3,12 +3,26 @@ package org.lukas.adventofcode2022.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class FileUtils {
     private static final String PREFIX = "src/main/resources/";
 
+    public static List<String> toLines(String relativePath) {
+        return toLines(relativePath, false);
+    }
+
     public static String toString(String relativePath) {
         return toString(relativePath, false);
+    }
+
+    public static List<String> toLines(String filePath, boolean isAbsolute) {
+        try {
+            var fullPath = isAbsolute ? filePath : PREFIX + filePath;
+            return Files.readAllLines(Paths.get(fullPath));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     public static String toString(String filePath, boolean isAbsolute) {
