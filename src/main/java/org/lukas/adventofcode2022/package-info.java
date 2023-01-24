2995007688b068eb0eb14fb6cde7a0d027090a41
Martin Lukas <martinlukas24@protonmail.com>
2023-01-24 00:59:05 +0000

    Add NonNull annotation by default

---
+++ src/main/java/org/lukas/adventofcode2022/package-info.java
@@ -0,0 +1,9 @@
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.FIELD)
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.PARAMETER)
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.RETURN)
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.LOCAL_VARIABLE)
+package org.lukas.adventofcode2022;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.checkerframework.framework.qual.TypeUseLocation;
