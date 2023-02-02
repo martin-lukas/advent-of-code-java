c2e0b0285666611008c8b81132f5ef9025b529bb
Martin Lukas <martinlukas24@protonmail.com>
2023-02-02 12:10:10 +0000

    Day 5 part 2 solution + refactor

--- src/main/java/org/lukas/adventofcode/year22/day5/SupplyStacks.java
+++ src/main/java/org/lukas/adventofcode/year22/day5/SupplyStacks.java
@@ -2,33 +2,30 @@ package org.lukas.adventofcode.year22.day5;
 
 import org.lukas.adventofcode.utils.FileUtils;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SupplyStacks {
     public static void main(String[] args) {
         var lines = FileUtils.inputToLines(22, 5);
-        List<LinkedList<Character>> stacks = createStacks(lines.subList(0, 8));
-        int[][] moves = createMoves(lines.subList(10, lines.size()));
+        List<int[]> moves = createMoves(lines.subList(10, lines.size()));
 
-        for (int[] move : moves) {
-            applyMove(stacks, move);
-        }
+        var stacksForSimpleMoves = createStacks(lines.subList(0, 8));
+        moves.forEach(move -> applySimpleMove(stacksForSimpleMoves, move));
 
-        System.out.print("Top containers for each stack: ");
-        stacks.stream()
-                .map(LinkedList::getLast)
-                .forEach(System.out::print);
-        System.out.println();
+        var stacksForBatchMoves = createStacks(lines.subList(0, 8));
+        moves.forEach(move -> applyBatchMove(stacksForBatchMoves, move));
+
+        printStackTops(stacksForSimpleMoves);
+        printStackTops(stacksForBatchMoves);
     }
 
-    static List<LinkedList<Character>> createStacks(List<String> stacksStrs) {
-        List<LinkedList<Character>> stacks = List.of(
-                new LinkedList<>(), new LinkedList<>(), new LinkedList<>(),
-                new LinkedList<>(), new LinkedList<>(), new LinkedList<>(),
-                new LinkedList<>(), new LinkedList<>(), new LinkedList<>()
+    static List<Deque<Character>> createStacks(List<String> stacksStrs) {
+        List<Deque<Character>> stacks = List.of(
+                new ArrayDeque<>(), new ArrayDeque<>(), new ArrayDeque<>(),
+                new ArrayDeque<>(), new ArrayDeque<>(), new ArrayDeque<>(),
+                new ArrayDeque<>(), new ArrayDeque<>(), new ArrayDeque<>()
         );
         for (String stacksStr : stacksStrs) {
             for (int col = 0, c = 1;
@@ -44,26 +41,42 @@ public class SupplyStacks {
         return stacks;
     }
 
-    static int[][] createMoves(List<String> movesStrs) {
+    static List<int[]> createMoves(List<String> movesStrs) {
         Pattern movePattern = Pattern.compile("move (\\d+) from (\\d+) to (\\d+)");
-        int[][] moves = new int[movesStrs.size()][3];
-        for (int i = 0; i < movesStrs.size(); i++) {
-            Matcher moveMatcher = movePattern.matcher(movesStrs.get(i));
+        List<int[]> moves = new ArrayList<>();
+        for (String movesStr : movesStrs) {
+            Matcher moveMatcher = movePattern.matcher(movesStr);
             if (moveMatcher.matches()) {
-                moves[i] = new int[] {
+                moves.add(new int[]{
                         Integer.parseInt(moveMatcher.group(1)),
                         Integer.parseInt(moveMatcher.group(2)),
                         Integer.parseInt(moveMatcher.group(3)),
-                };
+                });
             }
         }
         return moves;
     }
 
-    static void applyMove(List<LinkedList<Character>> stacks, int[] move) {
+    static void applySimpleMove(List<Deque<Character>> stacks, int[] move) {
         for (int i = 0; i < move[0]; i++) {
             Character container = stacks.get(move[1] - 1).removeLast();
             stacks.get(move[2] - 1).addLast(container);
         }
+    }
+
+    static void applyBatchMove(List<Deque<Character>> stacks, int[] move) {
+        LinkedList<Character> containers = new LinkedList<>();
+        for (int i = 0; i < move[0]; i++) {
+            containers.addFirst(stacks.get(move[1] - 1).removeLast());
+        }
+        stacks.get(move[2] - 1).addAll(containers);
+    }
+
+    static void printStackTops(List<Deque<Character>> stacks) {
+        System.out.print("Top containers for each stack: ");
+        stacks.stream()
+                .map(Deque::getLast)
+                .forEach(System.out::print);
+        System.out.println();
     }
 }
