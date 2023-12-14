a3e996be90238d659f7ce9c09c0e6c60c0ce76e0
Martin Lukas <martin.lukas@email.com>
2023-12-14 03:44:00 +0000

    Add trimming to file reading

--- src/main/java/org/lukas/adventofcode/utils/FileUtils.java
+++ src/main/java/org/lukas/adventofcode/utils/FileUtils.java
@@ -6,40 +6,43 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class FileUtils {
-    private static final String PREFIX = "src/main/resources/";
-    private static final String REL_PATH = "year%d/day%d/input.txt";
 
-    public static List<String> inputToLines(int year, int day) {
-        return toLines(REL_PATH.formatted(year, day));
-    }
+  private static final String PREFIX = "src/main/resources/";
+  private static final String REL_PATH = "year%d/day%d/input.txt";
 
-    public static String inputToString(int year, int day) {
-        return toString(REL_PATH.formatted(year, day));
-    }
+  public static List<String> inputToLines(int year, int day) {
+    return toLines(REL_PATH.formatted(year, day));
+  }
 
-    public static List<String> toLines(String relativePath) {
-        return toLines(relativePath, false);
-    }
+  public static String inputToString(int year, int day) {
+    return toString(REL_PATH.formatted(year, day));
+  }
 
-    public static String toString(String relativePath) {
-        return toString(relativePath, false);
-    }
+  public static List<String> toLines(String relativePath) {
+    return toLines(relativePath, false);
+  }
 
-    public static List<String> toLines(String filePath, boolean isAbsolute) {
-        try {
-            var fullPath = isAbsolute ? filePath : PREFIX + filePath;
-            return Files.readAllLines(Paths.get(fullPath));
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+  public static String toString(String relativePath) {
+    return toString(relativePath, false);
+  }
 
-    public static String toString(String filePath, boolean isAbsolute) {
-        try {
-            var fullPath = isAbsolute ? filePath : PREFIX + filePath;
-            return Files.readString(Paths.get(fullPath));
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+  public static List<String> toLines(String filePath, boolean isAbsolute) {
+    try {
+      var fullPath = isAbsolute ? filePath : PREFIX + filePath;
+      return Files.readAllLines(Paths.get(fullPath)).stream()
+          .filter(String::isBlank)
+          .toList();
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
     }
+  }
+
+  public static String toString(String filePath, boolean isAbsolute) {
+    try {
+      var fullPath = isAbsolute ? filePath : PREFIX + filePath;
+      return Files.readString(Paths.get(fullPath)).trim();
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
 }
