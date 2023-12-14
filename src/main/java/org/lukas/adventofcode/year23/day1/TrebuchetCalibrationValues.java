e5d2d99ec1dd1d24f9b17cb16b5223e7cd1c92aa
Martin Lukas <martin.lukas@email.com>
2023-12-14 03:43:33 +0000

    Prepare for 2023

---
+++ src/main/java/org/lukas/adventofcode/year23/day1/TrebuchetCalibrationValues.java
@@ -0,0 +1,15 @@
+package org.lukas.adventofcode.year23.day1;
+
+import org.lukas.adventofcode.utils.FileUtils;
+
+class TrebuchetCalibrationValues {
+
+  public static void main(String[] args) {
+    var lines = FileUtils.inputToLines(22, 1);
+
+    var res1 = "todo";
+
+    System.out.println("Part 1: " + res1);
+    //System.out.println("Part 2: " + res2);
+  }
+}
