859c61307c5c827cea772d4a376b87086293118a
Martin Lukas <martin.lukas@email.com>
2023-12-27 17:07:32 +0100

    Prepare for Y23-D4

---
+++ src/main/java/org/lukas/adventofcode/year23/Day4.java
@@ -0,0 +1,16 @@
+package org.lukas.adventofcode.year23;
+
+import static org.lukas.adventofcode.utils.Logger.log;
+
+import org.lukas.adventofcode.utils.FileUtils;
+
+/**
+ * <a href="https://adventofcode.com/2023/day/4">Day 4: Scratchcards</a>
+ */
+class Day4 {
+
+  public static void main(String[] args) {
+    var x = FileUtils.inputToString(23, 4);
+    log.info(STR."X: \{x}");
+  }
+}
---
+++ src/test/java/org/lukas/adventofcode/year23/Day4Test.java
@@ -0,0 +1,12 @@
+package org.lukas.adventofcode.year23;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Day4Test {
+
+  @Test
+  void testX() {
+  }
+}
