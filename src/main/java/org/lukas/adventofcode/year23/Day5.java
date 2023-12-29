ffb33621ce4666dce44a9f9dab49f7c74a341d8b
Martin Lukas <martin.lukas@email.com>
2023-12-29 11:18:40 +0000

    Prepare for Y23-D5

---
+++ src/main/java/org/lukas/adventofcode/year23/Day5.java
@@ -0,0 +1,20 @@
+package org.lukas.adventofcode.year23;
+
+import static org.lukas.adventofcode.utils.Logger.log;
+
+import org.lukas.adventofcode.utils.FileUtils;
+
+/**
+ * <a href="https://adventofcode.com/2023/day/5">Day 5: If You Give A Seed A Fertilizer</a>
+ */
+class Day5 {
+
+  public static void main(String[] args) {
+    var input = FileUtils.inputToString(23, 5);
+    log.info(STR."Result: \{x(input)}");
+  }
+
+  static int x(String input) {
+    return 0;
+  }
+}
